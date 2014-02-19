d780dd191059953c4f638d4d8595cece54ad3ac1
aaleksander <aleksandranufriev@yandex.ru>
2014-02-19 22:46:44 +0400

    1

---
+++ text.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-
+
+from py2gcode import *
+from py2gcode.Fonts import arial
+
+sz = 12
+def f():
+    G0(0, 0, sz)
+    font = get_font("arial")
+    t = TextTrajectory(font, "И в а н  В я ч е с л а в о в и ч")
+    for tt in t.trajectories:
+        tt['svg'].mirror_y()
+    t.grav(136, 8.75, 0, sz, 500, 0.8) #0.4
+    G0(Z=sz)
+    G0(0, 0)
+
+
+preview(f)
+    
